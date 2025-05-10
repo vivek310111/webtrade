6feb4d010de675fc9dd50a504939266b0f361412
vivek310111 <vivekjinadra31@gmail.com>
2025-05-11 04:26:52 +0530

    home page import error

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from '../src/pages/Home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/register'
 import Listing from './pages/Listing'
